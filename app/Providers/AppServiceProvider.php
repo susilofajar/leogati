34af788f53ac454e766bcad26086ebac860d040d
susilofajar <susilofajar220@gmail.com>
2026-08-26 00:29:57 +0700

    Force HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (app()->environment('production')) {
+    URL::forceScheme('https');
     }
 }
